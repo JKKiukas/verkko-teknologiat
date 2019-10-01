e67d2cf189b9c64a87474b00a3e2a600f5b73f07
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-10-01 21:01:08 +0300

    Luvuilla voi laskea plus-, miinus-, jako- ja kertolaskun. Lukuja ei voi pienentää pienemmiksi kuin 1 eikä suuremmiksi kuin 10.

--- js/laskin.js
+++ js/laskin.js
@@ -9,22 +9,61 @@ function decreaseFirstValue() {
     var value = parseInt(document.getElementById("getFirstNumber").value);
     value--;
     document.getElementById("getFirstNumber").value = value;
+
+    if (value < 2) {
+        document.getElementById("firstButtonMinus").disabled = true;
+    }
 }
 
 function increaseFirstValue() {
     var value = parseInt(document.getElementById("getFirstNumber").value);
     value++;
     document.getElementById("getFirstNumber").value = value;
+
+    if (value > 9) {
+        document.getElementById("firstButtonPlus").disabled = true;
+    }
 }
 
 function decreaseSecondValue() {
     var value = parseInt(document.getElementById("getSecondNumber").value);
     value--;
     document.getElementById("getSecondNumber").value = value;
+
+    if (value < 2) {
+        document.getElementById("secondButtonMinus").disabled = true;
+    }
 }
 
 function increaseSecondValue() {
     var value = parseInt(document.getElementById("getSecondNumber").value);
     value++;
     document.getElementById("getSecondNumber").value = value;
+
+    if (value > 9) {
+        document.getElementById("secondButtonPlus").disabled = true;
+    }
+}
+
+function calculateNumbers() {
+    var firstNumber = document.getElementById("getFirstNumber").value;
+    var secondNumber = document.getElementById("getSecondNumber").value;
+    var value = document.getElementById("selectOperator").value;
+
+    if (value === "plusSign") {
+        console.log(parseInt(firstNumber) + parseInt(secondNumber));
+        document.getElementById("result").value = parseInt(firstNumber) + parseInt(secondNumber);
+    }
+    if (value === "minusSign") {
+        console.log(firstNumber - secondNumber);
+        document.getElementById("result").value = parseInt(firstNumber) - parseInt(secondNumber);
+    }
+    if (value === "multiplyingSign") {
+        console.log(firstNumber * secondNumber);
+        document.getElementById("result").value = parseInt(firstNumber) * parseInt(secondNumber);
+    }
+    if (value === "divisionSign") {
+        console.log(firstNumber / secondNumber);
+        document.getElementById("result").value = parseInt(firstNumber) / parseInt(secondNumber);
+    }
 }
