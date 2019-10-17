8eeb9caf6572b6ee87f027110acbe9cbd520a903
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-10-17 16:56:28 +0300

    Kuvan ja tekstin pystyy pysäyttämään ja laittamaan takaisin päälle. Kuvan vaihtuessa myös teksti vaihtuu ja kertoo kuvan nimen.

--- js/esitys.js
+++ js/esitys.js
@@ -1,5 +1,5 @@
 var nextPicture = 0;
-var stopPicture;
+var playPicture;
 automaticShow();
 
 function automaticShow() {
@@ -9,9 +9,29 @@ function automaticShow() {
         x[i].style.display = "none";
     }
     nextPicture++;
-    if (nextPicture > x.length) {nextPicture = 1}
-    x[nextPicture-1].style.display = "block";
-    stopPicture = setTimeout(automaticShow, 2000);
+    if (nextPicture > x.length) {
+        nextPicture = 1
+    }
+    x[nextPicture - 1].style.display = "block";
+    playPicture = setTimeout(automaticShow, 2000);
+}
+
+var nextText = 0;
+var playText;
+automaticText();
+
+function automaticText() {
+    var j;
+    var z = document.getElementsByClassName("nameOfCat");
+    for (j = 0; j < z.length; j++) {
+        z[j].style.display = "none";
+    }
+    nextText++;
+    if (nextText > z.length) {
+        nextText = 1
+    }
+    z[nextText - 1].style.display = "block";
+    playText = setTimeout(automaticText, 2000);
 }
 
 var slidePic = 1;
@@ -36,11 +56,42 @@ function displayPicture(pic) {
     getPicture[slidePic - 1].style.display = "block";
 }
 
-function pausePicture() {
-    clearTimeout(stopPicture);
+function pausePlayPicture() {
+    if (playPicture != null) {
+        clearTimeout(playPicture);
+        playPicture = null;
+    } else {
+        playPicture = setTimeout(automaticShow, 2000)
+    }
 }
 
+function pausePlayText() {
+    if (playText != null) {
+        clearTimeout(playText);
+        playText = null;
+    } else {
+        playText = setTimeout(automaticText, 2000)
+    }
+}
 
-function playPicture() {
-    setTimeout(automaticShow, 2000);
+var textSlide = 1;
+showText(textSlide);
+
+function newText(x) {
+    showText(textSlide += x);
+}
+
+function showText(x) {
+    var i;
+    var slides = document.getElementsByClassName("nameOfCat");
+    if (x > slides.length) {
+        textSlide = 1
+    }
+    if (x < 1) {
+        textSlide = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[textSlide - 1].style.display = "block";
 }
